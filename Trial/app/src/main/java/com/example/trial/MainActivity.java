e2fd2170c80351a776d727d0feba2e6af24d1f33
Raghav16071 <raghav16071@iiitd.ac.in>
2019-08-12 20:57:54 +0530

    Add files via upload

---
+++ Trial/app/src/main/java/com/example/trial/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.trial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void performAction(View view) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+    }
+}
+
